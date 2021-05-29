fd9ea6aeb4ad9359fa19a96efa4d08f18b8f867c
Elagabalus <angelsantander609@gmail.com>
2021-05-29 11:46:42 -0500

    Tablas completadas

--- controller/analisis_exploratorio/__init__.py
+++ controller/analisis_exploratorio/__init__.py
@@ -1,11 +1,13 @@
 from .preview_table import PreviewTableController
 from .struct_table import StructTableController
+from .faltantes_table_controller import FaltantesTableController
 class AnalisisExploratorioController(PreviewTableController,
-    StructTableController):
+    StructTableController,FaltantesTableController):
     def set_analisis_exploratorio(self):
         if self.file:
             self.scroll_analisis.show()
             self.set_preview_table()
             self.set_struct_table()
+            self.set_missed_table()
         else:
             self.scroll_analisis.hide()
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-from controller.analisis_exploratorio.preview_table import PreviewTableController
 from model.load_csv import datos
 def print_detail(objeto):
     print(objeto,type(objeto),sep=",")
-sub_dataframe=datos.head(10)
-print_detail(sub_dataframe)
+
+# sub_dataframe=datos.head(10)
+# print_detail(sub_dataframe)
 
 def data_preview(datos,n_rows):
     for i in range(len(datos.columns)):
@@ -11,3 +11,10 @@ def data_preview(datos,n_rows):
         print(column_name)
         for j in range(n_rows):
             print(datos[column_name][j])
+def data_missed(datos):
+    missed=datos.isnull().sum()
+    cols=datos.axes[1]
+    # print_detail(missed)
+    for i in range(len(missed)):
+        print_detail(cols[i])
+        print_detail(missed[i])
